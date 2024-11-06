0dfef3ae2d62b00f1e57539403a708aefcf326f8
srinidhi2 <gantasrinidhi@gmail.com>
2024-11-06 11:09:10 -0600

    commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -11,6 +11,8 @@ import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
 
+import imageheader from './imageheader.png'
+
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
 import food_3 from './food_3.png'
